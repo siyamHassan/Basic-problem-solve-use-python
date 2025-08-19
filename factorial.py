f1df06eb8bd8db48f8dfd47e359fa10e9a647848
siyamHassan <ratkim158@gmail.com>
2025-08-19 07:52:31 -0700

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,21 @@
+u = int(input("Entre number:"))
+def Cheeker(r):
+    factorial = 1
+    for a in range(1,r+1):
+        factorial=factorial*a
+    return factorial
+ans = Cheeker(u)
+print(ans)
+
+
+
+
+
+
+
+
+
+
+
+
+
