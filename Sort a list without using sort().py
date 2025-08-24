530f1fc9bf69a6e376529a2f69bb238fdb46830f
siyamHassan <ratkim158@gmail.com>
2025-08-24 09:49:08 -0700

    Add files via upload

---
+++ Sort a list without using sort().py
@@ -0,0 +1,9 @@
+import string
+low = string.ascii_lowercase
+thislist = ["orange", "mango", "kiwi", "pineapple", "banana"]
+new = []
+for a in low:
+    for b in thislist:
+        if b.startswith(a):
+            new.append(b)
+print(new)
